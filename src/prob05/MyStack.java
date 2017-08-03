0ef00e16e1b129206799af26b5467e9096586200
YeonJin <duswls0515@gmail.com>
2017-08-03 13:13:24 +0900

    commit

---
+++ src/prob05/MyStack.java
@@ -0,0 +1,23 @@
+package prob05;
+
+public class MyStack {
+	
+	//생성자
+	public MyStack() {
+		//기본생성자
+	}
+	
+	public MyStack(int num) {
+		//num의 갯수만큼 기본적으로 배열을 생성한다.
+	}
+	
+	//isEmpty
+	
+	//push
+	
+	//pop
+	//null 이라면 
+	
+	//MyStackException
+
+}
